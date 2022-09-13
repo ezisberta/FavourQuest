96830719ebfe6b92a6661a0981533afd64b7d427
EddTheDuck <eddisaduck@hotmail.com>
2022-09-13 15:16:22 +0100

    Starting questlog

--- App/screens/QuestLog.js
+++ App/screens/QuestLog.js
@@ -1,8 +1,119 @@
-import React from "react";
-import { View } from "react-native-web";
+import { useEffect, useState } from "react";
+import {
+  Text,
+  ImageBackground,
+  StyleSheet,
+  View,
+  Pressable,
+} from "react-native";
+import { useFonts } from "expo-font";
+import colors from "../config/colors";
+import LoadingPage from "./LoadingPage";
+import { auth, db } from "../../firebase";
 
-function QuestLog(props) {
-  return <View></View>;
+function QuestLog({ navigation }) {
+  const [username, setUsername] = useState("");
+
+  const [fontsLoaded] = useFonts({
+    "Minecraft-Bold": require("../assets/fonts/minecraft-font/Minecraft-Bold.otf"),
+    "Minecraft-Regular": require("../assets/fonts/minecraft-font/Minecraft-Regular.otf"),
+    "Minecraft-Italic": require("../assets/fonts/minecraft-font/Minecraft-Italic.otf"),
+    "Minecraft-BoldItalic": require("../assets/fonts/minecraft-font/Minecraft-BoldItalic.otf"),
+  });
+
+  if (!fontsLoaded) {
+    return <LoadingPage />;
+  }
+
+  return (
+    <ImageBackground
+      source={require("../assets/images/Pixel1.png")}
+      style={styles.background}
+    >
+      <View style={styles.header}>
+        <Pressable
+          onPress={() => {
+            navigation.navigate("Map");
+          }}
+          style={styles.BackButtonBorder}
+        >
+          <Text style={styles.BackArrow}>⇤</Text>
+        </Pressable>
+      </View>
+    </ImageBackground>
+  );
 }
+const styles = StyleSheet.create({
+  ProfileCardContent: {
+    fontSize: "20",
+    fontFamily: "Minecraft-Bold",
+    color: colors.primary,
+  },
+  ProfileOuterCard: {
+    margin: 25,
+    height: 170,
+    paddingLeft: 25,
+    backgroundColor: colors.secondary,
+  },
+  ProfileCard: {
+    marginTop: 10,
+    fontFamily: "Minecraft-Regular",
+  },
+
+  Edit: {
+    height: 80,
+    backgroundColor: colors.primary,
+    margin: 25,
+  },
+  EditText: {
+    paddingTop: 18,
+    color: colors.white,
+    fontSize: 35,
+    textAlign: "center",
+    fontFamily: "Minecraft-Regular",
+  },
+
+  background: { flex: 1 },
+
+  ViewForm: {
+    paddingTop: "10%",
+  },
+
+  TextInput: {
+    color: colors.black,
+    textAlign: "center",
+    height: 40,
+    margin: 25,
+    borderWidth: 2,
+    backgroundColor: colors.white,
+    borderColor: colors.primary,
+    fontFamily: "Minecraft-Regular",
+  },
+  ProfileText: {
+    marginTop: 20,
+    marginLeft: 20,
+    textShadowColor: colors.black,
+    textShadowRadius: "10",
+    textAlign: "center",
+    color: colors.white,
+    fontSize: 40,
+    fontFamily: "Minecraft-Bold",
+  },
+  BackArrow: {
+    color: colors.white,
+    fontSize: 40,
+    textAlign: "center",
+    margin: -6,
+  },
+  BackButtonBorder: {
+    margin: 25,
+    width: 40,
+    height: 40,
+    backgroundColor: colors.primary,
+  },
+  header: {
+    flexDirection: "row",
+  },
+});
 
 export default QuestLog;
